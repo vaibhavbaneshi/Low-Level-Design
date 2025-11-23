7dee9ab2d880a69b480e30e71b3481dd918380f0
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2025-11-23 18:57:16 +0530

    OOPS -> Encapsulation

--- OOPS/Encapsulation/Encapsulation.java
+++ OOPS/Encapsulation/Encapsulation.java
@@ -1,5 +1,100 @@
 package Encapsulation;
+/*
+Car Interface --> Act as an interface for Outsiude world to operate the car. 
+This interface tells 'WHAT' all it can do rather then 'HOW' it does that.
+Since this is an interface we cannot directly create Objects of this. We
+need to implement it first and then that child class will have the responsibility to 
+provide implementation details of all the methods in the interface.
+
+In our real world example of Car, imagine you sitting in the car and able to operate
+the car (startEngine, accelerate, brake, turn) just by pressing or moving some
+pedals/buttons/stearing wheel etc. You dont need to know how these things work, and
+also they are hidden under thre hood.
+This Interface 'Car' denotes that (pedals/buttons/stearing wheel etc). 
+*/
+
+interface Car {
+    void startEngine();
+    void shiftGear();
+    void accelerate();
+    void brake();
+    void stopEngine();
+}
+
+/*
+This is a Concrete class (A class that provide implementation details of an interface/abstract class).
+Now anyone can make an Object of 'SportsCar' and can assign it to 'Car' reference. 
+(See main method for this)
+
+In our real world example of Car, as you cannot have a real car by just having its body only
+(all these buttons or pedals). You need to have the actual implementation of 'What' happens
+when we press these buttons. 'SportsCar' class denotes that actual implementation. 
+
+Hence we can concude, to denote a real world car in programming we created 2 classes.
+One to deonte all the user-interface like pedals, buttons, stearing wheels etc ('Car' interface).
+And another one to denote the actual car with all the implementations of these buttons (SportsCar' class).
+ */
+
+class SportsCar implements Car {
+
+    private String brand;
+    private String model;
+    private int mileage;
+    private int topSpeed; 
+    private int currentSpeed;
+    private int currentGear;
+    private boolean isEngineOn = false;
+
+    public SportsCar(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        currentSpeed = 0;
+        currentGear = 0;
+        isEngineOn = false;
+    }
+
+    //Created Setter and Getter Function
+    public void getter() {
+        System.out.println("Current Speed : " + this.currentSpeed);
+    }
+
+    public void setter(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void startEngine() { 
+        if(!isEngineOn) {
+            System.out.println("Engine is off.");
+            return;
+        }
+
+        isEngineOn = true;
+        System.out.println("Engine Started.");
+    }
+
+    public void shiftGear() {
+        System.out.println("Gear Shifted");
+    }
+
+    public void accelerate() {
+        System.out.println("Car Accelerated");
+    }
+
+    public void brake() {
+        System.out.println("Car brakes applied");
+    }
+
+    public void stopEngine() {
+        isEngineOn = false;
+        System.out.println("Car Engine Stopped");
+    }
+}
 
 public class Encapsulation {
-    
+    public static void main(String[] args) {
+        SportsCar obj = new SportsCar("Tata", "Curvv");
+        obj.startEngine();
+        obj.setter(100);
+        obj.getter();
+    }
 }
